7358c7b2696862d0695c265c17b8887e0a16f1c1
Ana <ajimenezl.crn@artsgrafiques.org>
2025-04-09 18:45:24 +0200

    api

---
+++ index.js
@@ -0,0 +1,21 @@
+const express = require('express');
+const app = express();
+const fs = require('fs');
+const PORT = process.env.PORT || 3000;
+
+// Ruta para mostrar los libros
+app.get('/libros', (req, res) => {
+  fs.readFile('./libros.json', 'utf8', (err, data) => {
+    if (err) {
+      res.status(500).json({ error: 'Error al leer el archivo' });
+      return;
+    }
+    const libros = JSON.parse(data);
+    res.json(libros);
+  });
+});
+
+// Iniciar el servidor
+app.listen(PORT, () => {
+  console.log(`Servidor corriendo en http://localhost:${PORT}`);
+});
